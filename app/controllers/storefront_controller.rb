2854433c55ccadbb509d150b431232e63202a52c
Jonathan <jonathandavidsewell@gmail.com>
2018-10-11 13:46:33 -0400

    further developed sorting functionality by category and brand

--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -5,8 +5,16 @@ class StorefrontController < ApplicationController
 
   def items_by_category
   	@categories = Category.all
+  	
   end
 
   def items_by_brand
+  	@products = Product.all
+  	@brands = []
+	@products.each do |product|
+  		if @brands.include?(product.brand) == false #keeps duplicates out
+  			@brands.push(product.brand)
+  		end
+  	end
   end
 end
